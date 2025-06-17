59949d2627d33c5fae1b80464b87af253dc17f09
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-06-17 10:17:22 +0300

    Completes Lab

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
-# server/app.py
+#!/usr/bin/env python3
 
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
-from models import db, User, Review, Game
+from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -15,13 +14,43 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return "Index for Game/Review/User API"
-
-# start building your API here
+    return "<h1>Bakery GET API</h1>"
 
 
-if __name__ == '__main__':
+@app.route("/bakeries")
+def bakeries():
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response(bakeries, 200)
+    return response
+
+
+@app.route("/bakeries/<int:id>")
+def bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    response = make_response(bakery.to_dict(), 200)
+    return response
+
+
+@app.route("/baked_goods/by_price")
+def baked_goods_by_price():
+    baked_goods = [
+        good.to_dict() for good in BakedGood.query.order_by(BakedGood.price.desc())
+    ]
+    response = make_response(baked_goods, 200)
+    return response
+
+
+@app.route("/baked_goods/most_expensive")
+def most_expensive_baked_good():
+    baked_goods = [
+        good.to_dict() for good in BakedGood.query.order_by(BakedGood.price.desc())
+    ]
+    response = make_response(baked_goods[0], 200)
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
